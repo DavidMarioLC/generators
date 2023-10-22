3dba726b86ffea2701450ba8b32e523f79ff6958
David Mario Licla Carpio <david@MacBook-Pro-de-David.local>
2023-10-22 16:16:37 -0500

    :bug: [FIX] change number value to string

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,11 +18,11 @@ const config: Config = {
       },
       keyframes: {
         meteor: {
-          "0%": { transform: "rotate(215deg) translateX(0)", opacity: 1 },
-          "70%": { opacity: 1 },
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1 " },
+          "70%": { opacity: "1" },
           "100%": {
             transform: "rotate(215deg) translateX(-500px)",
-            opacity: 0,
+            opacity: "0",
           },
         },
       },
